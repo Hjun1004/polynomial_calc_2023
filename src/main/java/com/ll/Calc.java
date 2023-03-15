8bd6afaf427fc55563881686ad8f5af2d94b2f04
Hjun1004 <hjun1004@naver.com>
2023-03-15 16:16:06 +0900

    (10 + 20) * 3 == 90 내가한거

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -38,16 +38,21 @@ public class Calc {
                 }
             }
 
+
+
             String firstExp = exp.substring(0, splitPointIndex+1);
-            System.out.println(exp);
-            System.out.println(firstExp);
+            //System.out.println(exp);
+            String middle = exp.substring(splitPointIndex+1,splitPointIndex+4);
+            //System.out.println(firstExp);
             String secondExp = exp.substring(splitPointIndex+4);
             // 이렇게 하면 ) 이후 부터 나오는 " + "가 아닌 뒤부터 들어간다.
+            if(middle.contains("+")) return Calc.run(firstExp) + Calc.run(secondExp);
+            else if(middle.contains("*")) return Calc.run(firstExp) * Calc.run(secondExp);
 
-            return Calc.run(firstExp) + Calc.run(secondExp);
         }
 
-            /*exp = exp.replaceAll("- ", "+ -");
+        else if(needToCompound) {
+            exp = exp.replaceAll("- ", "+ -");
             String[] compound_bits = exp.split(" \\+ "); //
 
             String newExp = Arrays.stream(compound_bits) // 안에 들어있는 각각의 요소를 스트림으로 바꾼다.
@@ -55,8 +60,9 @@ public class Calc {
                     .mapToObj(e -> e + "") // run에서 return받은 값들을 ""로 감싸서 문자열들로 바꾸고
                     .collect(Collectors.joining( " + ")); // 그 문자열들을 " + " 더하기를 붙이면서 하나하나 조합해 최종 문자열을 만든다.
 
-            return run(newExp);*/
+            return run(newExp);
             //return Integer.parseInt(compound_bits[0])+Calc.run(compound_bits[1]);
+        }
 
         else if(needToPlus){
             exp = exp.replaceAll("- ", "+ -");
